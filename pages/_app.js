6f85616e0601282a458fb389923656401b1d082b
ronix-transformhub <ronaldoo@transformhub.com.au>
2021-10-22 10:57:36 +0800

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import "../styles/globals.css";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
